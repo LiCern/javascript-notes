51d6e606d5d7734ec81bcf844e1d18a818043d49
Lisa Cernilogar <56543648+LiCern@users.noreply.github.com>
2020-04-07 09:24:15 +0100

    Update notes.js

--- notes.js
+++ notes.js
@@ -14,5 +14,13 @@ const people = [
    }
  ];
  
+ const sortedByAge = people.sort(function(a, b)) {
+
+//NEGATIVE VALUE = ASCENDING ORDER (when this function returns a negative value that means that a is going to be before b in the finalised sorted array)
+//POSITIVE VALUE = DESCENDING ORDER (when it returns a positive value it means that b will be above a in the finalised sorted array)
+// 0 = UNCHANGED (leaves a and b unchanged with respect to each other)                                 
+   
+                                 return a.age - b.age; //ascending order, to sort it in a descending order we would have written (b.age - a.age)                               
  
+                                 });
    
